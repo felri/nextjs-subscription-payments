a2410040e12ae8cfbbe11a2b2597582accfa7c91
felri <felipercmelo@gmail.com>
2023-09-04 14:47:17 -0300

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import DisableRightClick from '@/components/DisableRightClick';
 import Toastify from '@/components/Toastify';
 import Footer from '@/components/ui/Footer';
 import Navbar from '@/components/ui/Navbar';
+import { Analytics } from '@vercel/analytics/react';
 import { PropsWithChildren, Suspense } from 'react';
 import 'styles/main.css';
 
@@ -62,6 +63,7 @@ export default function RootLayout({
             {children}
           </main>
           <Footer />
+          <Analytics />
         </SupabaseProvider>
       </body>
     </html>
