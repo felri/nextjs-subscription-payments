e84fce0fe22944f48ee1330d9a7e3d41b78a92b9
felri <felipercmelo@gmail.com>
2023-09-19 12:10:09 -0300

    metadata profile

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,8 +1,45 @@
 import Seller from './Seller';
 import { getSellerProfile, getAllServiceTags } from '@/app/supabase-server';
 import { Database } from '@/types_db';
+import { getStorageSupabaseUrl } from '@/utils/helpers';
+import { Metadata } from 'next';
 import { redirect } from 'next/navigation';
 import React, { Suspense } from 'react';
+
+interface Props {
+  params: { id: string };
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  // read route params
+  const { id } = params;
+  const seller = await getSellerProfile(id);
+
+  const url = `https://primabela.lol/profile/${id}`;
+  const title = `${seller?.name || ''} | Primabela`;
+  const description = `${seller?.short_description || ''}`;
+  const image = `${getStorageSupabaseUrl(seller?.featured_image || '', id)}`;
+
+  return {
+    title: title,
+    description: description,
+    robots: 'follow, index',
+    openGraph: {
+      url: url,
+      title: title,
+      description: description,
+      images: [
+        {
+          url: image,
+          width: 1200,
+          height: 630,
+          alt: title
+        }
+      ],
+      siteName: title
+    }
+  };
+}
 
 export default async function SearchBarPage({
   params
